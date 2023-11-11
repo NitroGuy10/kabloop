fc6a64763f5bee3a15f2a3dccdc3a17508158a18
Adam Teo <adam.teo321@gmail.com>
2023-11-11 13:40:49 -0600

    font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,13 @@
   let variable = 1;
 </script>
 
-<div class="flex justify-center items-center">
+<div class="flex justify-center items-center font-satoshi">
   <div class="mt-64">
-    <p class="text-7xl font-bold text-start">Learn more. Do more.</p>
+    <p class="text-7xl font-black text-start">Learn more. Do more.</p>
     <p class="text-2xl font-medium text-start opacity-70 w-2/3 mt-4">Generate tests question from a picture and compete with others to learn the content.</p>
     <div class="flex justify-start gap-4 mt-8">
-      <a href="/lobby" class="bg-blue-500 px-8 py-3 rounded-md text-white font-medium min-w-[200px] text-center">Join Lobby</a>
-      <a href="/ocr" class="border-2 border-blue-500 bg-white px-8 py-3 rounded-md font-medium min-w-[200px] text-center">Upload Notes</a>
+      <a href="/lobby" class="bg-blue-500 px-8 py-3 rounded-md text-white font-bold min-w-[200px] text-center">Join Lobby</a>
+      <a href="/ocr" class="border-2 border-blue-500 bg-white px-8 py-3 rounded-md font-bold min-w-[200px] text-center">Upload Notes</a>
     </div>
   </div>
   <img alt="alt" src="/images/phone.png" class="object-cover h-[400px] w-[200px] rounded-lg mt-32 translate-y-12" />
--- src/routes/lobby/+page.svelte
+++ src/routes/lobby/+page.svelte
@@ -29,10 +29,10 @@
 
 <svelte:window bind:innerWidth bind:outerWidth bind:innerHeight bind:outerHeight />
 
-<div class="h-screen overflow-hidden">
+<div class="h-screen overflow-hidden font-satoshi">
   {#if innerWidth > 1024}
     <div class="flex flex-col justify-center w-screen h-[60px] bg-gray-200">
-      <p class="text-3xl text-center font-medium">
+      <p class="text-3xl text-center font-extrabold">
         {question}
       </p>
     </div>
@@ -40,13 +40,13 @@
       <img src={images[randomNum]} class="object-cover h-[400px] w-[800px] rounded-lg" alt="img" />
     </div>
 
-    <div class="mt-16 w-screen px-8 grid grid-cols-2 grid-rows-2 gap-4">
+    <div class="mt-16 w-screen px-8 grid grid-cols-2 grid-rows-2 gap-4 font-bold">
       <div id="button1" class="bg-red-200 p-4 rounded-md">Box 1</div>
       <div id="button2" class="bg-blue-200 p-4 rounded-md">Box 2</div>
       <div id="button3" class="bg-green-200 p-4 rounded-md">Box 3</div>
       <div id="button4" class="bg-yellow-200 p-4 rounded-md">Box 4</div>
     </div>
-    <div class="flex justify-center gap-4 mt-12">
+    <div class="flex justify-center gap-4 mt-12 font-bold">
       <button class="rounded-md px-4 py-2 border-gray-700 bg-slate-500 text-white">
         Next
       </button>
@@ -64,7 +64,7 @@
     <div class="flex justify-center my-12">
       <img src={images[0]} class="object-cover h-[200px] w-1/2 rounded-lg" alt="img" />
     </div>
-    <div class="flex flex-col gap-4 mx-4">
+    <div class="flex flex-col gap-4 mx-4 font-bold">
       <button id="button1" class="bg-red-200 p-4 rounded-md text-center">Option 1</button>
       <button id="button2" class="bg-blue-200 p-4 rounded-md text-center">Option 2</button>
       <button id="button3" class="bg-green-200 p-4 rounded-md text-center">Option 3</button>
